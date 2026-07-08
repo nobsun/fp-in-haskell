a7b72f328e878ad989aae1208bf7c3f31bf57c55
Nobuo Yamashita <nobsun@sampou.org>
2026-07-08 18:33:59 +0900

    雛形Lib.hs改訂

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,20 +1,32 @@
 -- # Lib
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE LexicalNegation #-}
-{-# LANGUAGE NPlusKPatterns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+-- ## 練習問題
+-- S-combinator、K-combinator、I-conbinator、B-combinator、C-combinator、それぞれのコンビネータの型シグネチャを確認せよ。
+-- ```
+-- S f g x = f x (g x)
+-- K x y = x
+-- I x = x
+-- B f g x = f (g x)
+-- C f x y = f y x
+-- ```
+--
+{-# LANGUAGE GHC2024 #-}
 module Lib
-    ( someFunc
+    (
     ) where
+-- | S-combinator、K-combinator、I-conbinator、B-combinator、C-combinator
+--
+--
+_S :: (a -> b -> c) -> (a -> b) -> a -> c
+_S = (<*>)
 
-{- $setup
->>> :set -XOverloadedStrings
--}
-{- | someFunc
-「なんか関数」を標準出力に印字する
->>> someFunc
-なんか関数
--}
-someFunc :: IO ()
-someFunc = putStrLn "なんか関数"
+_K :: a -> b -> a
+_K = const
+
+_I :: a -> a
+_I = id
+
+_B :: (b -> c) -> (a -> b) -> a -> c
+_B = (.)
+
+_C :: (a -> b -> c) -> b -> a -> c
+_C = flip
