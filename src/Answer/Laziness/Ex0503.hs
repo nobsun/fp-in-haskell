31ee4108e2188c06cb7c43946dbb97b7fc1f63aa
Nobuo Yamashita <nobsun@sampou.org>
2026-07-10 12:56:30 +0900

    練習問題 5.1 追加

--- src/Answer/Laziness/Ex0503.hs
+++ src/Answer/Laziness/Ex0503.hs
@@ -1,32 +1,21 @@
 -- # Answer.Laziness.Ex0503
--- ## 練習問題
--- S-combinator、K-combinator、I-conbinator、B-combinator、C-combinator、それぞれのコンビネータの型シグネチャを確認せよ。
--- ```
--- S f g x = f x (g x)
--- K x y = x
--- I x = x
--- B f g x = f (g x)
--- C f x y = f y x
--- ```
+-- ## 練習問題 5.3
 --
 {-# LANGUAGE GHC2024 #-}
 module Answer.Laziness.Ex0503
     (
     ) where
--- | S-combinator、K-combinator、I-conbinator、B-combinator、C-combinator
 --
+-- | _takeWhile
+-- >>> _takeWhile odd [3,1,4,1,5,9]
+-- [3,1]
+-- >>> _takeWhile (5 >) [3,1,4,1,5,9]
+-- [3,1,4,1]
+-- >>> _takeWhile even [3,1,4,1,5,9]
+-- []
 --
-_S :: (a -> b -> c) -> (a -> b) -> a -> c
-_S = (<*>)
-
-_K :: a -> b -> a
-_K = const
-
-_I :: a -> a
-_I = id
-
-_B :: (b -> c) -> (a -> b) -> a -> c
-_B = (.)
-
-_C :: (a -> b -> c) -> b -> a -> c
-_C = flip
+_takeWhile :: (a -> Bool) -> [a] -> [a]
+_takeWhile p = \ case
+    [] -> []
+    x:xs | p x       -> x : _takeWhile p xs
+         | otherwise -> []
