560c435f1ee350ff3f962df180947aa3fa240bce
Nobuo Yamashita <nobsun@sampou.org>
2026-07-10 19:37:27 +0900

    練習問題 5.14 追加および練習問題 5.13 修正

--- src/Answer/Laziness/Ex0513.hs
+++ src/Answer/Laziness/Ex0513.hs
@@ -1,23 +1,49 @@
 -- # Answer.Laziness.Ex0513
 -- ## 練習問題 5.13
--- 指定したリストがもうひとつのリストの接頭辞になっているかを判定する
--- `isPrefixOf :: Eq a => [a] -> [a] -> Bool`を実装せよ。
+-- `unfoldr`を用いて`map`、`take`、`takeWhile`、`zipWith`、`zipAll`を実装せよ。
+-- `zipAll`は両方のリストが尽きるまでとじあわせる。
+-- `zipAll :: [a] -> [b] -> [(Maybe a, Maybe b)]
 --
 {-# LANGUAGE GHC2024 #-}
+{-# LANGUAGE NPlusKPatterns #-}
 module Answer.Laziness.Ex0513
     (
     ) where
--- | `isPrefixOf`は基本モジュール`Data.List`の関数
--- >>> "abra" `_isPrefixOf` "abracadabra"
--- True
--- >>> "adab" `_isPrefixOf` "abracadabra"
--- False
--- >>> "abracadabra" `_isPrefixOf` "abra"
--- False
-_isPrefixOf :: Eq a => [a] -> [a] -> Bool
-_isPrefixOf = \ case
-    []   -> const True
-    x:xs -> \ case
-        [] -> False
-        y:ys | x == y    -> _isPrefixOf xs ys
-             | otherwise -> False
+import Data.List ( unfoldr )
+
+_map :: (a -> b) -> [a] -> [b]
+_map f = unfoldr psi where
+    psi = \ case
+        []   -> Nothing
+        x:xs -> Just (f x, xs)
+
+_take :: Int -> [a] -> [a]
+_take n xs = unfoldr psi (n,xs) where
+    psi = \ case
+        (m+1,aas) -> case aas of
+            []        -> Nothing
+            a:as      -> Just (a,(m,as))
+        _         -> Nothing
+
+_takeWhile :: (a -> Bool) -> [a] -> [a]
+_takeWhile p = unfoldr psi where
+    psi = \ case
+        []   -> Nothing
+        x:xs | p x       -> Just (x,xs)
+             | otherwise -> Nothing
+
+_zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+_zipWith f xs ys = unfoldr psi (xs,ys) where
+    psi = \ case
+        ([],_)      -> Nothing
+        (_,[])      -> Nothing
+        (a:as,b:bs) -> Just (f a b, (as,bs))
+
+_zipAll :: [a] -> [b] -> [(Maybe a, Maybe b)]
+_zipAll xs ys = unfoldr psi (xs,ys) where
+    psi = \ case
+        (a:as,b:bs) -> Just ((Just a,  Just b),  (as, bs))
+        ([],b:bs)   -> Just ((Nothing, Just b),  ([], bs))
+        (a:as,[])   -> Just ((Just a , Nothing), (as, []))
+        _           -> Nothing
+        
